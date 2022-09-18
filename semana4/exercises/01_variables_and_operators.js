e45098d57880b4d2ef77a210202bceb1cd464e54
lucasMusre <musrelucas@gmail.com>
2022-09-18 15:22:41 -0300

    BaSP-W04-2022:done ejercise 1

---
+++ semana4/exercises/01_variables_and_operators.js
@@ -0,0 +1,16 @@
+// Variables y Operadores
+//1.a//
+var firstNumber = 27 ;
+var secondNumber =  30;
+var result  = firstNumber + secondNumber
+console.log('Exercise 1.a):', result);
+//1.b//
+var  firstName = 'Lucas';
+var secondName = 'Andres';
+var fullName = firstName + secondName ;
+console.log ('Exercise 1.a):', fullName );
+//1.c//
+var  today = 'tuesday' ;
+var tomorrow = 'Wednesday';
+var firstDaysOfTheWeek = today.length + tomorrow.length;
+console.log ('Exercise 1.c):', firstDaysOfTheWeek );
