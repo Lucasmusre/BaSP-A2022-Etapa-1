4cfe44441c1553824eee6e2f5911fa0da671444c
lucasMusre <musrelucas@gmail.com>
2022-09-24 17:04:56 -0300

    BaSP-W06-2022:add folders

--- semana4/exercises/06_functions.js
+++ semana4/exercises/06_functions.js
@@ -6,27 +6,22 @@ function suma(a, b){
     sumaTotal = suma(3,5)
 console.log('Exercise 6.a: ' + sumaTotal)
 //A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número; de no ser un número, mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
-function sumaB(a,b) {
+sumaTotal = sumaB(3,5)
+function suma(a,b) {
     if (isNaN(a) || isNaN(b)){
         alert('Exercise 6.b): ' + 'Parameter Error\nIts not a number');
         return NaN;
     }
     return a + b;
 }
-myVarA = sumaB(1,2)
-    
+
 //Aparte, crear una función validate Integer que reciba un número como parámetro y devuelva verdadero si es un número entero.
 function validateInteger(i){
     if (Number.isInteger(i)){
-
         return true;
     }
     return false;
-        return console.log('Exercise 6.c): ' + 'The number ' + i + ' its integer');
     }
-    console.log('Exercise 6.c): ' + 'The number ' + i + ' its not integer');
-}
-i = validateInteger(10)
+i = validateInteger(8)
 //A la función suma del ejercicio 6b) agregarle una llamada a la función del ejercicio 6c. y que valide que los números sean enteros. En caso que haya decimales mostrar un alerta con el error y retornar el número convertido a entero (redondeado).
-
 //Convertir la validación del ejercicio 6d) en una función separada y llamarla dentro de la función suma probando que todo siga funcionando igual.
